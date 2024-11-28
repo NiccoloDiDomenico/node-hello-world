a22ca4138d24b777b2bc6de74d237b20e7c02750
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-28 16:22:30 +0100

    feat: complete exercise

---
+++ app.js
@@ -0,0 +1,13 @@
+let message = `Hello World`;
+console.log(message);
+
+message = `Hello Boolean`;
+console.log(message);
+
+const argoments = process.env.LOGNAME
+console.log(argoments);
+
+message = `Hello ${process.env.LOGNAME}`
+console.log(message);
+
+
